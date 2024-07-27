5e1b6863f4e3e870683055abc160bc07defab3c2
mindl3ss <justtodesign@gmail.com>
2024-07-27 21:08:44 +0200

    feat: initial commit with dependencies setup

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@pinia/nuxt"
+  ],
+
+  css: ['@/assets/css/tailwind.css'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+
+  compatibilityDate: "2024-07-27"
+})
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+const loading = ref(false)
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <div class="flex flex-col gap-y-4 items-center justify-center">
+      <a href="https://nuxt.com" target="_blank" aria-label="Nuxt">
+        <svg width="61" height="42" viewBox="0 0 61 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+        </svg>
+      
+      </a>
+      <h1 class="text-black dark:text-white text-4xl sm:text-5xl font-semibold text-center">I am going to write in  Nuxt!</h1>
+    </div>
+  </div>
+</template>
