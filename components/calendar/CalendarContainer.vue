e8c2f34853b5f57bab24ad464b754f77508bfeb5
mindl3ss <justtodesign@gmail.com>
2024-07-29 15:40:47 +0200

    fix: update calendarContainer to use internationalization package

--- components/calendar/CalendarContainer.vue
+++ components/calendar/CalendarContainer.vue
@@ -1,44 +1,53 @@
 <script setup lang="ts">
-import { format } from 'date-fns'
-import { Calendar as CalendarIcon } from 'lucide-vue-next'
-
 import { ref } from 'vue'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/v-calendar'
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
-import { useSettingsStore } from '@/stores/settings'
+  CalendarDate,
+  DateFormatter,
+  type DateValue,
+  getLocalTimeZone,
+  today,
+  parseDateTime
+} from '@internationalized/date'
+import { Calendar as CalendarIcon } from 'lucide-vue-next'
+import { Calendar } from '@/components/ui/calendar'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
 
-const { updatedSince, setUpdatedSince } = useSettingsStore()
-const open = ref(false)
-const date = ref<Date>(new Date(updatedSince))
-
-watch(date, (newValue) => {
-  date.value = newValue;
-  setUpdatedSince(format(newValue, 'yyyy-MM-dd'))
+// Create the date formatter
+const df = new DateFormatter('en-US', {
+  dateStyle: 'long',
 })
+
+const settingsStore = useSettingsStore()
+const { updatedSince, setUpdatedSince } = settingsStore
+
+// Convert the string to a DateValue
+// this is needed as Calendar only works with Internationalized format
+const value = ref(parseDateTime(updatedSince)) as Ref<DateValue>
+
+watch(value, () => {
+  setUpdatedSince(value.value.toLocaleString().substring(0, 10))
+})
+
 </script>
 
 <template>
   <Popover>
     <PopoverTrigger as-child>
       <Button
-        :variant="'outline'"
+        variant="outline"
         :class="cn(
           'w-[280px] justify-start text-left font-normal',
-          !date && 'text-muted-foreground',
+          !value && 'text-muted-foreground',
         )"
       >
         <CalendarIcon class="mr-2 h-4 w-4" />
-        <span>{{ date ? format(date, "PPP") : "Pick a date" }}</span>
+        {{ value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date" }}
       </Button>
     </PopoverTrigger>
     <PopoverContent class="w-auto p-0">
-      <Calendar v-model="date" click-to-close />
+      <Calendar v-model="value" initial-focus />
     </PopoverContent>
   </Popover>
 </template>
